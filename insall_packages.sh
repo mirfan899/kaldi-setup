5c7902a3578b553c06d0b609e9f0f08f4cebb2be
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-10-02 11:34:49 +0500

    more packages.

--- insall_packages.sh
+++ insall_packages.sh
@@ -2,10 +2,11 @@
 apt-get update
 apt-get install build-essential software-properties-common -y
 apt-get install libssl-dev libffi-dev python-dev
+apt-get install automake autoconf gfortran subversion
 apt-get install python3-venv -y
 apt-get install gcc g++
 apt-get install flac swig
-apt-get install sox automake autoconfig subversion unzip
+apt-get install sox automake autoconfig unzip
 apt-get install python-setuptools python3-pip
 apt-get install -y curl git libc-dev libreadline-dev libtool make ncurses-dev pkg-config
 apt-get wavpack wget zlib1g-dev
